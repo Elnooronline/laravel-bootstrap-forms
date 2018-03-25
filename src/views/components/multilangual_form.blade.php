532110b996d65eb85c5583594efed83e263411d3
Ahmed Al-Shahawi <alshahawi@outlook.com>
2018-03-25 22:45:09 +0200

    Update component usage of uniqid

--- src/views/components/multilangual_form.blade.php
+++ src/views/components/multilangual_form.blade.php
@@ -1,10 +1,9 @@
-@php($id = uniqid('__multilangual-form-', true))
 <div>
     <!-- Nav tabs -->
     <ul class="nav nav-tabs" role="tablist">
         @multilangualForm
         <li role="presentation" class="{{ $loop->index == 0 ? 'active' : '' }}">
-            <a href="#{{ $id.$locale->code }}" aria-controls="{{ $id.$locale->code }}" role="tab" data-toggle="tab">
+            <a href="#{{ $uniqid.$locale->code }}" aria-controls="{{ uniqid.$locale->code }}" role="tab" data-toggle="tab">
                 {{ $locale->name }}
             </a>
         </li>
@@ -14,7 +13,7 @@
     <!-- Tab panes -->
     <div class="tab-content" style="margin-top: 10px;">
         @multilangualForm
-        <div role="tabpanel" class="tab-pane{{ $loop->index == 0 ? ' active' : '' }}" id="{{ $id.$locale->code }}">
+        <div role="tabpanel" class="tab-pane{{ $loop->index == 0 ? ' active' : '' }}" id="{{ $uniqid.$locale->code }}">
             @stack($uniqid.$locale->code)
         </div>
         @endMultilangualForm
