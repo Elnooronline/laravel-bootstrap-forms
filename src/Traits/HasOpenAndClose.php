3bf608629e2e68c98abc261d0ec8ca06cdca9a82
Ahmed Fathy <aliraqi.dev@gmail.com>
2018-04-10 14:27:44 +0200

    add model feature to fill attributes automaticly

--- src/Traits/HasOpenAndClose.php
+++ src/Traits/HasOpenAndClose.php
@@ -94,6 +94,51 @@ trait HasOpenAndClose
     }
 
     /**
+     * Open the form tag with model and put method.
+     *
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @param $url
+     * @param array $attributes
+     * @return \Illuminate\Support\HtmlString
+     */
+    public function model($model, $url, $attributes = [])
+    {
+        return $this->putModel($model, $url, $attributes);
+    }
+
+    /**
+     * Open the form tag with model and put method.
+     *
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @param $url
+     * @param array $attributes
+     * @return \Illuminate\Support\HtmlString
+     */
+    public function putModel($model, $url, $attributes = [])
+    {
+        return Form::model($model, array_merge($attributes, [
+            'url' => $url,
+            'method' => 'put',
+        ]));
+    }
+
+    /**
+     * Open the form tag with model and patch method.
+     *
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @param $url
+     * @param array $attributes
+     * @return \Illuminate\Support\HtmlString
+     */
+    public function patchModel($model, $url, $attributes = [])
+    {
+        return Form::model($model, array_merge($attributes, [
+            'url' => $url,
+            'method' => 'patch',
+        ]));
+    }
+
+    /**
      * Close the form tag.
      *
      * @return \Illuminate\Support\HtmlString
